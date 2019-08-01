e4f046bdfb7c09716ee64122271d2b9be35d1530
Shahrukh Mansuri <mansurishahrukh007@gmail.com>
2019-08-02 02:13:32 +0530

    Contract modified, purchase ticket implemented

--- contracts/TicketCreation_simplified.sol
+++ contracts/TicketCreation_simplified.sol
@@ -1,10 +1,10 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-contract TicketCreation {
+contract TicketCreationSimplified {
 
   /* Emit events when new accounts and new tickets have been created */
   event NewAccount(uint256 indexed userId, string indexed firstName, string indexed lastName);
-  event NewTicket(uint256 indexed ticketId, string indexed eventName, string indexed description, bool available, uint16 price);
+  event NewTicket(uint256 indexed ticketId, string indexed eventName, string indexed description, uint16 price);
 
   /* Struct for User */
   struct User {
@@ -17,12 +17,8 @@ contract TicketCreation {
   struct Ticket {
     string eventName;
     string description;
-    bool available;
     uint16 price;
   }
-
-  /* Determines how much ether a seller should stake */
-  uint16 stakingFactor = 4;
 
   User[] users; /* Array of Users */
   Ticket[] public tickets; /* Array of Tickets */
@@ -33,13 +29,6 @@ contract TicketCreation {
   mapping (uint256 => address) ticketsToOwner;
   /* Maps users to their number of tickets held */
   mapping (address => uint16) ownerToQuantity;
-  /* Maps ticket IDs to approved buyers */
-  mapping (uint256 => address) approvedBuyers;
-
-  function staking(uint16 _price) internal {
-      uint256 stakeAmount = _price * 1 ether/stakingFactor;
-      require(msg.value == stakeAmount, "Your staking amount is incorrect."); /* Requires seller to stake ether */
-  }
 
   function accountCreation(string calldata _firstName, string calldata _lastName) external {
     uint256 userId = users.push(User(msg.sender, _firstName, _lastName));
@@ -47,15 +36,12 @@ contract TicketCreation {
     emit NewAccount(userId, _firstName, _lastName); /* Event emitter */
   }
 
-  function createTicket(string calldata _eventName, string calldata _description, uint16 _price) external payable {
+  function createTicket(string calldata _eventName, string calldata _description, uint16 _price) external {
     require(adToUserId[msg.sender] > 0, "Please create an account first."); /* Requires user to have an account */
-    staking(_price);
-    // uint256 stakeAmount = _price * 1 ether / stakingFactor;
-    //require(msg.value == stakeAmount, "Your staking amount is incorrect."); /* Requires seller to stake ether */
-    uint256 ticketId = tickets.push(Ticket(_eventName, _description, true, _price))-1;
+    uint256 ticketId = tickets.push(Ticket(_eventName, _description, _price))-1;
     ticketsToOwner[ticketId] = msg.sender;
     ownerToQuantity[msg.sender]++;
-    emit NewTicket(ticketId, _eventName, _description, true, _price); /* Event emitter */
+    emit NewTicket(ticketId, _eventName, _description, _price); /* Event emitter */
   }
 
 }
--- src/js/app.js
+++ src/js/app.js
@@ -4,18 +4,18 @@ App = {
 
   init: async function () {
     $.getJSON('../tickets.json', function (data) {
-      var ticketsRow = $('#ticketsRow');
-      var ticketTemplate = $('#ticketTemplate');
+      // var ticketsRow = $('#ticketsRow');
+      // var ticketTemplate = $('#ticketTemplate');
 
-      for (i = 0; i < data.length; i++) {
-        ticketTemplate.find('img').attr('src', data[i].picture);
-        ticketTemplate.find('.ticket-event').text(data[i].event);
-        ticketTemplate.find('.ticket-description').text(data[i].description);
-        ticketTemplate.find('.ticket-price').text(data[i].price);
-        ticketTemplate.find('.btn-purchase').attr('data-id', data[i].id);
+      // for (i = 0; i < data.length; i++) {
+      //   ticketTemplate.find('img').attr('src', data[i].picture);
+      //   ticketTemplate.find('.ticket-event').text(data[i].event);
+      //   ticketTemplate.find('.ticket-description').text(data[i].description);
+      //   ticketTemplate.find('.ticket-price').text(data[i].price);
+      //   ticketTemplate.find('.btn-purchase').attr('data-id', data[i].id);
 
-        ticketsRow.append(ticketTemplate.html());
-      }
+      //   ticketsRow.append(ticketTemplate.html());
+      // }
     });
 
     return await App.initWeb3();
@@ -47,13 +47,49 @@ App = {
   },
 
   initContract: function () {
-    $.getJSON('TicketTransfer.json', function (data) {
+    $.getJSON('TicketTransferSimplified.json', function (data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract
       var TicketTransferArtifact = data;
       App.contracts.TicketTransfer = TruffleContract(TicketTransferArtifact);
 
       // Set the provider for our contract
       App.contracts.TicketTransfer.setProvider(App.web3Provider);
+
+      App.contracts.TicketTransfer.deployed().then(function (instance) {
+        ticketTransferInstance = instance;
+
+        return ticketTransferInstance.getTicketCount.call();
+      }).then(async function (ticketCounts) {
+        var ticketsRow = $('#ticketsRow');
+        var ticketTemplate = $('#ticketTemplate');
+        $.getJSON('../tickets.json', function (data) {
+          for (i = 0; i < ticketCounts; i++) {
+            ticketTemplate.find('img').attr('src', data[i].picture);
+            ticketTemplate.find('.ticket-event').text(data[i].event);
+            ticketTemplate.find('.ticket-description').text(data[i].description);
+            ticketTemplate.find('.ticket-price').text(data[i].price);
+            ticketTemplate.find('.btn-purchase').attr('data-id', data[i].id);
+            ticketsRow.append(ticketTemplate.html());
+          }
+        });
+
+        web3.eth.getAccounts(async function (error, accounts) {
+          if (error) {
+            console.log(error);
+          }
+          var to = accounts[0];
+          for (let i = 0; i < ticketCounts; i++) {
+            let ownerAddress = await ticketTransferInstance.ownerOf(i);
+            if (ownerAddress === to) {
+              $('.panel-ticket').eq(i).find('.btn-purchase').text('Purchased').attr('disabled', true);
+            }
+            console.log(i, ownerAddress);
+          }
+        });
+
+      }).catch(function (err) {
+        console.log(err.message);
+      });
     });
 
     return App.bindEvents();
@@ -67,27 +103,43 @@ App = {
     event.preventDefault();
 
     var ticketID = parseInt($(event.target).data('id'));
+    console.log('[ticketID]', ticketID);
 
     var ticketTransferInstance;
 
-    web3.eth.getAccounts(function (error, accounts) {
-      if (error) {
-        console.log(error);
-      }
+    $.getJSON('../tickets.json', function (data) {
+      var ticketPrice = data[ticketID].priceEther;
+      web3.eth.getAccounts(function (error, accounts) {
+        if (error) {
+          console.log(error);
+        }
 
-      var account = accounts[0];
-
-      // App.contracts.TicketTransfer.deployed().then(function (instance) {
-      //   ticketTransferInstance = instance;
-
-      //   // Execute adopt as a transaction by sending account
-      //   return ticketTransferInstance.adopt(ticketID, { from: account });
-      // }).then(function (result) {
-      //   return App.markAdopted();
-      // }).catch(function (err) {
-      //   console.log(err.message);
-      // });
+        var to = accounts[0];
+        console.log('[to]', to);
+        App.contracts.TicketTransfer.deployed().then(function (instance) {
+          ticketTransferInstance = instance;
+          return ticketTransferInstance.getContractDeployer.call();
+        }).then(function (ticketOwner) {
+          console.log('[ticket owner]', ticketOwner);
+          return ticketTransferInstance.transferFrom(ticketOwner, to, ticketID, {
+            from: to,
+            value: ticketPrice * 1000000000000000000
+          });
+        }).then(async function (response) {
+          console.log('[transferFrom response]', response);
+          for (let i = 0; i < 7; i++) {
+            let ownerAddress = await ticketTransferInstance.ownerOf(i);
+            if (ownerAddress === to) {
+              $('.panel-ticket').eq(i).find('.btn-purchase').text('Purchased').attr('disabled', true);
+            }
+            console.log(i, ownerAddress);
+          }
+        }).catch(function (err) {
+          console.log(err.message);
+        });
+      });
     });
+
   }
 
 };
